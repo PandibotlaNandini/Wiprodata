bb5be818d4f3bad741619fc5f288c1aef901e740
PandibotlaNandini <nandinipandibotla2003@gmail.com>
2025-07-21 18:55:32 +0530

    First Commit

---
+++ Helloworld/Helloworld/Bitwise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Bitwise
+    {
+        static void Main()
+        {
+            int a = 5, b = 3;
+                Console.WriteLine(a | b);
+        }
+
+    }
+}
+
