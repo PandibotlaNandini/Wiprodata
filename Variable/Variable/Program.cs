c014a9276d9d82e0b64d5a5248320c4057be8673
PandibotlaNandini <nandinipandibotla2003@gmail.com>
2025-07-21 18:52:58 +0530

    first

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a = 12;
+
+            string name = "Prasanna";
+
+            double basic = 84235.22;
+
+            Console.WriteLine("A value is  " + a);
+
+            Console.WriteLine("Name is  " + name);
+
+            Console.WriteLine("Salary is  " + basic);
+        }
+    }
+}
